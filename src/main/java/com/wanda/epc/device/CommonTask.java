d147f2864bf60c0b7f23078cb443fac14182efb0
孙率众 <334837455@qq.com>
2023-08-11 09:18:09 +0800

    每分钟自动清除报警

--- src/main/java/com/wanda/epc/device/CommonTask.java
+++ src/main/java/com/wanda/epc/device/CommonTask.java
@@ -1,9 +1,7 @@
-
 package com.wanda.epc.device;
 
 import com.wanda.epc.param.DeviceMessage;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -26,24 +24,20 @@ import java.util.Map;
 public class CommonTask {
 
     @Autowired
-    private BaseDevice baseDevice;
-
-    @Autowired
     CommonDevice commonDevice;
-
+    @Autowired
+    private BaseDevice baseDevice;
 
     @Scheduled(cron = "0/60 * * * * ?")
     public boolean processData() throws Exception {
         log.info("子系统不返回报警恢复指令，自动处理");
         Iterator<Map.Entry<String, List<DeviceMessage>>> iterator = baseDevice.deviceParamListMap.entrySet().iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             Map.Entry<String, List<DeviceMessage>> next = iterator.next();
-            if(next.getKey().endsWith("_isAlarm")){
-                for(DeviceMessage deviceMessage: next.getValue()){
-                    if(StringUtils.isEmpty(deviceMessage.getValue())){
-                        deviceMessage.setValue("0");
-                        commonDevice.sendMessage(deviceMessage);
-                    }
+            if (next.getKey().endsWith("_isAlarm")) {
+                for (DeviceMessage deviceMessage : next.getValue()) {
+                    deviceMessage.setValue("0");
+                    commonDevice.sendMessage(deviceMessage);
                 }
             }
         }
