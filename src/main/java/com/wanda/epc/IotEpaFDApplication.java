c5603cc8702c2b940a01026140a00cbdecf35b94
孙率众 <334837455@qq.com>
2024-03-27 11:10:18 +0800

    使能列表读自配置文件

--- src/main/java/com/wanda/epc/IotEpaFDApplication.java
+++ src/main/java/com/wanda/epc/IotEpaFDApplication.java
@@ -4,6 +4,7 @@ import com.wanda.epc.device.HikVisionEasDevice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
@@ -12,13 +13,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class IotEpaFDApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IotEpaFDApplication.class, args);
-        log.info("使能开始，子系统号：{}", 1);
-        HikVisionEasDevice.subsystemParamEx(1);
-        log.info("使能结束，子系统号：{}", 1);
-        log.info("使能开始，子系统号：{}", 2);
-        HikVisionEasDevice.subsystemParamEx(2);
-        log.info("使能结束，子系统号：{}", 2);
+        ConfigurableApplicationContext context = SpringApplication.run(IotEpaFDApplication.class, args);
+        String group = context.getEnvironment().getProperty("epc.group");
+        String[] groups = group.split(",");
+        if (groups.length == 0) {
+            return;
+        }
+        for (String arg : groups) {
+            log.info("使能开始，子系统号：{}", arg);
+            HikVisionEasDevice.subsystemParamEx(Integer.valueOf(arg));
+            log.info("使能结束，子系统号：{}", arg);
+        }
     }
 
 }
