17d31ea45a8e98bf2deb76a1b93df1efcef977ad
孙率众 <334837455@qq.com>
2024-06-19 09:52:55 +0800

    点位对应关系逻辑优化

--- src/main/java/com/wanda/epc/device/DeviceHandler.java
+++ src/main/java/com/wanda/epc/device/DeviceHandler.java
@@ -52,12 +52,23 @@ public class DeviceHandler extends BaseDevice {
     /**
      * 子系统集合
      */
-    Set<String> subSystemSet = new HashSet<>();
+    Set<Object> subSystemIndexSet = new HashSet<>();
     /**
      * 防区集合
      */
-    Set<String> defenceSet = new HashSet<>();
-
+    Set<Object> defenceIndexSet = new HashSet<>();
+    /**
+     * 防区编码与防区通道号对应
+     */
+    Map<Object, Object> defenceIndexCode2NoMap = new HashMap<>();
+    /**
+     * 防区通道号与子系统编码对应
+     */
+    Map<Object, Object> defenceNo2SubSystemIndexCodeMap = new HashMap<>();
+    /**
+     * 子系统编码与防区通道号对应
+     */
+    Map<Object, Set<Object>> subSystemIndexCode2DefenceNoMap = new HashMap<>();
     @Value("${epc.host}")
     private String host;
     @Value("${epc.appKey}")
@@ -66,6 +77,8 @@ public class DeviceHandler extends BaseDevice {
     private String appSecret;
     @Value("${epc.search}")
     private String search;
+    @Value("${epc.searchDefence}")
+    private String searchDefence;
     @Value("${epc.control}")
     private String control;
     @Value("${epc.subSysStatus}")
@@ -106,36 +119,67 @@ public class DeviceHandler extends BaseDevice {
         // post请求Form表单参数
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("pageNo", "1");
-        paramMap.put("pageSize", "200");
+        paramMap.put("pageSize", "30");
         String result = sendHttps(search, paramMap);
         log.info("接口:{},参数:{},返回:{}", search, JSONObject.toJSONString(paramMap), result);
-        deviceParamListMap.forEach((key, value) -> {
-            if (key.contains(DEPLOY_WITH_DRAW_ALARM_SET)) {
-                subSystemSet.add(key.split("_")[0]);
-            } else {
-                defenceSet.add(key.split("_")[0]);
+        List<Map<String, Object>> list = (List<Map<String, Object>>) JSONPath.read(result, "$.data.list");
+        if (CollectionUtils.isEmpty(list)) {
+            log.error("接口返回值为空");
+            return;
+        }
+        for (Map<String, Object> map : list) {
+            String indexCode = String.valueOf(map.get("indexCode"));
+            Map<String, Object> paramMap2 = new HashMap<>();
+            paramMap2.put("deviceIndexCode", indexCode);
+            String result2 = sendHttps(searchDefence, paramMap2);
+            log.info("接口:{},参数:{},返回:{}", searchDefence, JSONObject.toJSONString(paramMap2), result2);
+            List<Map<String, Object>> list2 = (List<Map<String, Object>>) JSONPath.read(result2, "$.data.list");
+            if (CollectionUtils.isEmpty(list2)) {
+                log.error("接口返回值为空");
+                return;
             }
-        });
+            for (Map<String, Object> map2 : list2) {
+                Object defenceNo = map2.get("defenceNo");
+                Object defenceIndexCode = map2.get("defenceIndexCode");
+                Object subSystemIndexCode = map2.get("subSystemIndexCode");
+                defenceIndexCode2NoMap.put(defenceIndexCode, defenceNo);
+                defenceNo2SubSystemIndexCodeMap.put(defenceNo, subSystemIndexCode);
+                if (subSystemIndexCode2DefenceNoMap.containsKey(subSystemIndexCode)) {
+                    subSystemIndexCode2DefenceNoMap.get(subSystemIndexCode).add(defenceNo);
+                } else {
+                    Set<Object> set = new HashSet<>();
+                    set.add(defenceNo);
+                    subSystemIndexCode2DefenceNoMap.put(subSystemIndexCode, set);
+                }
+                subSystemIndexSet.add(subSystemIndexCode);
+                defenceIndexSet.add(defenceIndexCode);
+            }
+        }
     }
 
     @Override
     public boolean processData() {
-        get();
-        get2();
+        getDefence();
+        getSubSys();
         return true;
     }
 
-    private void get() {
+    /**
+     * 获取防区状态
+     */
+    private void getDefence() {
         Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put("defenceIndexCodes", defenceSet);
+        paramMap.put("defenceIndexCodes", defenceIndexSet);
         String result = sendHttps(defenceStatus, paramMap);
         log.info("接口:{},参数:{},返回:{}", defenceStatus, JSONObject.toJSONString(paramMap), result);
         List<Map<String, Object>> list = (List<Map<String, Object>>) JSONPath.read(result, "$.data.list");
         if (CollectionUtils.isEmpty(list)) {
+            log.error("接口返回值为空");
             return;
         }
         for (Map<String, Object> map : list) {
-            String defenceIndexCode = String.valueOf(map.get("defenceIndexCode"));
+            Object defenceNo = defenceIndexCode2NoMap.get(map.get("defenceIndexCode"));
+            log.info("获取防区状态:{}", defenceNo);
             //防区状态：-1: 未知 0: 离线 1: 正常 2: 故障 3: 报警 4: 旁路
             String status = String.valueOf(map.get("status"));
             String onlineStatus = "1", faultStatus = "0", isAlarm = "0";
@@ -146,25 +190,32 @@ public class DeviceHandler extends BaseDevice {
             } else if ("3".equals(status)) {
                 isAlarm = "1";
             }
-            sendMsg(defenceIndexCode + ONLINE_STATUS, onlineStatus);
-            sendMsg(defenceIndexCode + FAULT_STATUS, faultStatus);
-            sendMsg(defenceIndexCode + IS_ALARM, isAlarm);
+            sendMsg(defenceNo + ONLINE_STATUS, onlineStatus);
+            sendMsg(defenceNo + FAULT_STATUS, faultStatus);
+            sendMsg(defenceNo + IS_ALARM, isAlarm);
         }
     }
 
-    private void get2() {
+    /**
+     * 获取子系统状态
+     */
+    private void getSubSys() {
         Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put("subSysIndexCodes", subSystemSet);
+        paramMap.put("subSysIndexCodes", subSystemIndexSet);
         String result = sendHttps(subSysStatus, paramMap);
         log.info("接口:{},参数:{},返回:{}", subSysStatus, JSONObject.toJSONString(paramMap), result);
         List<Map<String, Object>> list = (List<Map<String, Object>>) JSONPath.read(result, "$.data.list");
         if (CollectionUtils.isEmpty(list)) {
+            log.error("接口返回值为空");
             return;
         }
         for (Map<String, Object> map : list) {
-            String subSystemIndexCode = String.valueOf(map.get("subSystemIndexCode"));
+            Set<Object> defenceNoSet = subSystemIndexCode2DefenceNoMap.get(map.get("subSystemIndexCode"));
+            log.info("获取子系统状态:{}", defenceNoSet);
             String status = String.valueOf(map.get("status"));
-            sendMsg(subSystemIndexCode + DEPLOY_WITH_DRAW_ALARM_SET_FEEDBACK, status);
+            for (Object defenceNo : defenceNoSet) {
+                sendMsg(defenceNo + DEPLOY_WITH_DRAW_ALARM_SET_FEEDBACK, status);
+            }
         }
     }
 
@@ -180,13 +231,19 @@ public class DeviceHandler extends BaseDevice {
             }
             redisUtil.set(outParamId, "0", 5);
             final String[] strings = deviceMessage.getOutParamId().split("_");
-            control(strings[0], value);
+            control(defenceNo2SubSystemIndexCodeMap.get(strings[0]), value);
         }
     }
 
-    public void control(String doorIndexCode, String value) {
+    /**
+     * 控制
+     *
+     * @param subSystemIndexCode
+     * @param value
+     */
+    public void control(Object subSystemIndexCode, String value) {
         Map<String, Object> map = new HashMap<>();
-        map.put("doorIndexCodes", doorIndexCode);
+        map.put("subSystemIndexCode", subSystemIndexCode);
         int status = 0;
         if ("1.0".equals(value)) {
             status = 1;
